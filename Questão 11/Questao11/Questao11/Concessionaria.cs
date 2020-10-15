0ddba85559f1b5945797f7f6b7b235873b89ba84
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-15 15:48:18 -0300

    Conclusao questao 11

---
+++ Questão 11/Questao11/Questao11/Concessionaria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questao11
+{
+    class Concessionaria
+    {
+        public double lucro, imposto, valor;
+
+        public void Calculos()
+        {
+            imposto = valor * 0.45;
+            lucro = valor * 0.12;
+        }
+    }
+}
---
+++ Questão 11/Questao11/Questao11/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Questao11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Concessionaria concessionaria = new Concessionaria();
+            Console.Write("Insira o valor do carro: ");
+            concessionaria.valor = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            concessionaria.Calculos();
+            Console.WriteLine($"O valor do imposto é: {concessionaria.imposto}");
+            Console.WriteLine($"O valor do lucro é: {concessionaria.lucro}");
+        }
+    }
+}
