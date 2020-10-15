830f265b5dd7b3fc59b0a95a88a08034339357d7
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-15 15:07:29 -0300

    Conclusão Questão 7

---
+++ Questão 7/Questao7/Questao7/Numero.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questao7
+{
+    class Numero
+    {
+        public double num1, num2, num3;
+        public String resultado;
+        public void Maior()
+        {
+            if (num1 > (num2 + num3)) {
+                resultado = $"O número {num1} é maior que a soma de {num2} e {num3}";
+            } else
+            {
+                resultado = $"O número {num1} não é maior que a soma de {num2} e {num3}";
+            }
+        }
+    }
+}
---
+++ Questão 7/Questao7/Questao7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Questao7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numero numero = new Numero();
+            Console.Write("Por favor, insira o valor do primeiro número: ");
+            numero.num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Por favor, insira o valor do segundo número: ");
+            numero.num2 = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Por favor, insira o valor do terceiro número: ");
+            numero.num3 = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            numero.Maior();
+            Console.WriteLine(numero.resultado);
+        }
+    }
+}
