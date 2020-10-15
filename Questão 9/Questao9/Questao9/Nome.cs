62dbe0e33bc8331d06493629457ce67dd01ba848
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-15 15:23:56 -0300

    Conclusão Questão 9

---
+++ Questão 9/Questao9/Questao9/Nome.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questao9
+{
+    class Nome
+    {
+        public String nome, resultado;
+        public void ConfereNome()
+        {
+            if (nome == "Carlos" || nome == "carlos")
+            {
+                resultado = "Nome correto";
+            }
+            else
+            {
+                resultado = "Nome incorreto";
+            }
+        }
+    }
+}
---
+++ Questão 9/Questao9/Questao9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Questao9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nome nome = new Nome();
+            Console.Write("Por favor, insira o nome: ");
+            nome.nome = Convert.ToString(Console.ReadLine());
+            Console.Clear();
+            nome.ConfereNome();
+            Console.WriteLine(nome.resultado);
+            
+        }
+    }
+}
