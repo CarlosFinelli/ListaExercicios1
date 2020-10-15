34f87b729fd61c6a1f2ae28b0316d3e174fbd275
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-15 14:49:45 -0300

    Conclusão questão 6

---
+++ Questão 6/Questão6/Questão6/Calculadora.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questão6
+{
+    class Calculadora
+    {
+        public double num1;
+        public double num2;
+        public double num3;
+        public double resultado;
+
+        public void Soma()
+        {
+            resultado = num1 + num2 + num3;
+        }
+
+        public void Multiplicacao()
+        {
+            resultado = num1 * num2 * num3;
+        }
+
+        public void Divisao()
+        {
+            resultado = (num1 / num2) / num3;
+        }
+    }
+}
---
+++ Questão 6/Questão6/Questão6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Questão6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calculadora = new Calculadora();
+            Console.Write("Por favor, insira o primeiro valor: ");
+            calculadora.num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Por favor, insira o segundo valor: ");
+            calculadora.num2 = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Por favor, insira o terceiro valor: ");
+            calculadora.num3 = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            calculadora.Soma();
+            Console.WriteLine($"A soma entre {calculadora.num1}, {calculadora.num2}, {calculadora.num3} é igual a: {calculadora.resultado}");
+            calculadora.Multiplicacao();
+            Console.WriteLine($"O produto entre {calculadora.num1}, {calculadora.num2} e {calculadora.num3} é igual a: {calculadora.resultado}");
+            calculadora.Divisao();
+            Console.WriteLine($"O Quociente entre {calculadora.num1}, {calculadora.num2} e {calculadora.num3} é igual a: {calculadora.resultado}");
+        }
+    }
+}
