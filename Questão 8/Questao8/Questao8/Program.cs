dc2ebe5fc5516027ca02cb82a708a25b7a3e3152
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-15 15:13:53 -0300

    Conclusão questão 8

---
+++ Questão 8/Questao8/Questao8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Questao8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Progressao progressao = new Progressao();
+            Console.Write("Insira a razão da progressão: ");
+            progressao.R = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Insira o primeiro termo da progressão: ");
+            progressao.X = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Insira o número de termos que deseja ter na progressão: ");
+            progressao.Y = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            progressao.Soma();
+            Console.WriteLine($"A soma de todos os itens da progressão é igual a: {progressao.X}");
+        }
+    }
+}
---
+++ Questão 8/Questao8/Questao8/Progressao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questao8
+{
+    class Progressao
+    {
+        public int R, X, Y;
+
+        public void Soma()
+        {
+            for (int i = 0; i < Y; i++)
+            {
+                X += R;
+            }
+        }
+    }
+}
