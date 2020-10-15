c3e66e2b3f10f3b68d928e5b3151460a57a6e7e4
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-15 15:38:21 -0300

    Conclusão questão 10

---
+++ Questão 10/Questao10/Questao10/Numero.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questao10
+{
+    class Numero
+    {
+        public double A, B;
+        public String resultado;
+        public void VerificaMaior()
+        {
+            if (A > B)
+            {
+                resultado = $"{A}";
+            }
+            if (A < B)
+            {
+                resultado = $"{B}";
+            }
+            if (A == B)
+            {
+                resultado = $"{A}={B}";
+            }
+        }
+    }
+}
---
+++ Questão 10/Questao10/Questao10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Questao10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numero numero = new Numero();
+            Console.Write("Por favor, insira o valor de A: ");
+            numero.A = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Por favor, insira o valor de B: ");
+            numero.B = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            numero.VerificaMaior();
+            {
+                Console.WriteLine(numero.resultado);
+            }
+        }
+    }
+}
