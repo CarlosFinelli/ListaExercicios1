5d8b7ddf35fc2fd3f042720c86f42007bfa303d3
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-15 11:41:51 -0300

    Questão 1 efetuada

---
+++ Questão 1/Questao1/Questao1/Sucessor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questao1
+{
+    class Sucessor
+    {
+        public int num;
+
+        public int sucessor()
+        {
+            num = ++num;
+            return num;
+        }
+    }
+}
