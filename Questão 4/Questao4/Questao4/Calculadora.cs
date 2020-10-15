68208e48c096c3ee352c196d3fa417bb600bbc8b
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-15 14:35:23 -0300

    Questão 4 finalizada

---
+++ Questão 4/Questao4/Questao4/Calculadora.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questao4
+{
+    class Calculadora
+    {
+        public int A;
+        public int B;
+        public double quociente1;
+        public double quociente2;
+        public int resto1;
+        public int resto2;
+        public void Divisao()
+        {
+            quociente1 = A / B;
+            quociente2 = B / A;
+        }
+        public void Resto()
+        {
+            resto1 = A % B;
+            resto2 = B % A;
+        }
+    }
+}
---
+++ Questão 4/Questao4/Questao4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Questao4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calculadora = new Calculadora();
+            Console.Write("Por favor, insira o valor do primeiro número: ");
+            calculadora.A = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Por favor, insira o valor do segundo número: ");
+            calculadora.B = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            calculadora.Divisao();
+            calculadora.Resto();
+            Console.WriteLine($"O quociente da divisão entre {calculadora.A} e {calculadora.B} é: {calculadora.quociente1}");
+            Console.WriteLine($"O resto da divisão entre {calculadora.A} e {calculadora.B} é: {calculadora.resto1}");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine($"O quociente da divisão entre {calculadora.B} e {calculadora.A} é: {calculadora.quociente2}");
+            Console.WriteLine($"O resto da divisão entre {calculadora.B} e {calculadora.A} é: {calculadora.resto2}");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+        }
+    }
+}
