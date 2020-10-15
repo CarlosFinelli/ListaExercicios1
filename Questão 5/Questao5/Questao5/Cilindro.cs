a3c45f81b2e90a7f0951e6c3df4c982a1fea764b
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-15 14:43:04 -0300

    Conclusão questão 5

---
+++ Questão 5/Questao5/Questao5/Cilindro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Text;
+
+namespace Questao5
+{
+    class Cilindro
+    {
+        public double aLateral;
+        public double aBase;
+        public double aTotal;
+
+        public void AreaTotal()
+        {
+            aTotal = aLateral + (2 * aBase);
+        }
+    }
+}
---
+++ Questão 5/Questao5/Questao5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Questao5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cilindro cilindro = new Cilindro();
+            Console.Write("Por favor, insira a base lateral do cilindro: ");
+            cilindro.aLateral = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Por favor, insira a área da base do cilindro: ");
+            cilindro.aBase = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            cilindro.AreaTotal();
+            Console.WriteLine($"A área total é igual a: {cilindro.aTotal}");
+        }
+    }
+}
