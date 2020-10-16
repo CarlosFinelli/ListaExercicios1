0e08536c7ab6e14286844e5448eff6605d6b3862
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-16 08:29:26 -0300

    Conclusão Questao 12

---
+++ Questão 12/Questao12/Questao12/Fiscalizacao.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questao12
+{
+    class Fiscalizacao
+    {
+        public int velocidadeMax, velocidadeMotorista;
+        public String retorno = String.Empty;
+
+        public void ValorMulta()
+        {
+            if (velocidadeMotorista <= velocidadeMax)
+            {
+                retorno = "Não há multa";
+            }
+            if (velocidadeMotorista > velocidadeMax && velocidadeMotorista <= velocidadeMax + 10)
+            {
+                retorno = "O valor da multa é de R$ 50,00";
+            }
+            if (velocidadeMotorista > velocidadeMax + 10 && velocidadeMotorista <= velocidadeMax + 30)
+            {
+                retorno = "O valor da multa é de R$ 100,00";
+            }
+            if (velocidadeMotorista > velocidadeMax + 31)
+            {
+                retorno = "O valor da multa é de R$ 200,00";
+            }
+        }
+    }
+}
---
+++ Questão 12/Questao12/Questao12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Questao12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fiscalizacao fiscalizacao = new Fiscalizacao();
+            Console.Write("Por favor, insira a velocidade máxima permitida: ");
+            fiscalizacao.velocidadeMax = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Por favor, insira a velocidade do motorista: ");
+            fiscalizacao.velocidadeMotorista = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            fiscalizacao.ValorMulta();
+            Console.WriteLine(fiscalizacao.retorno);
+        }
+    }
+}
