77a96a410ea0aa0d92982b4c2249949af935757e
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-21 17:00:23 -0300

    Conclusão questão 20

---
+++ Questão 20/Questao20/Questao20/Ano.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questao20
+{
+    class Ano
+    {
+        public int ano;
+
+
+        public String bissexto()
+        {
+            if (ano % 400 == 0 || ano % 4 == 0 && ano % 100 != 0)
+            {
+                return "Este ano é bissexto.";
+            }
+            else
+            {
+                return "Este ano não é bissexto.";
+            }
+        }
+    }
+}
---
+++ Questão 20/Questao20/Questao20/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Questao20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ano ano = new Ano();
+            Console.Write("Insira o ano que deseja verificar: ");
+            ano.ano = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine(ano.bissexto());
+        }
+    }
+}
