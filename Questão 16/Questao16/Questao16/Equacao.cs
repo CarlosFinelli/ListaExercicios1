a7651927d5f273afbf371f2e1dde269079cab3dc
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-22 11:38:21 -0300

    Conclusão questão 16

---
+++ Questão 16/Questao16/Questao16/Equacao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questao16
+{
+    class Equacao
+    {
+        public int A, B, C;
+        public double x1, x2;
+
+        public void Raiz()
+        {
+            x1 = (-B + Math.Sqrt(B * B - (4 * A * C))) / 2 * A;
+            x2 = (-B - Math.Sqrt(B * B - (4 * A * C))) / 2 * A;
+        }
+    }
+}
--- Questão 16/Questao16/Questao16/Program.cs
+++ Questão 16/Questao16/Questao16/Program.cs
@@ -6,7 +6,18 @@ namespace Questao16
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Equacao equacao = new Equacao();
+            Console.Write("Insira o valor de A: ");
+            equacao.A = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Insira o valor de B: ");
+            equacao.B = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Insira o valor de C: ");
+            equacao.C = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            equacao.Raiz();
+            Console.WriteLine($"As raizes dessa equação são: {equacao.x2} e {equacao.x1}. ");
         }
     }
 }
