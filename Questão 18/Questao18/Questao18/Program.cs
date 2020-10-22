17102da20121b8da10588c000562266c4980813f
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-22 10:09:23 -0300

    Conclusão questão 13

---
+++ Questão 18/Questao18/Questao18/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Questao18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Latao latao = new Latao();
+            Console.Write("Insira a quantidade de latão que deseja: ");
+            latao.qtd = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            latao.Total();
+            Console.WriteLine($"Para conseguir {latao.qtd} de latão, você precisará de {latao.cobre} de cobre e {latao.zinco} de zinco");
+        }
+    }
+}
